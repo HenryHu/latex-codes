50e59aae5d36bc2d074d1607efe850f43b9e2bbf
henry.hu.sh@gmail.com <henry.hu.sh@gmail.com@75d5e88c-67b2-8db1-2261-f24598e24071>
2012-05-12 09:24:05 +0000

    with more evaluation results

--- trunk/6824_project/test-barrier.c
+++ trunk/6824_project/test-barrier.c
@@ -4,7 +4,6 @@
 
 #define MAXPROCS 32
 pthread_t threads[MAXPROCS];
-
 pthread_barrier_t barrier;
 
 void* do_work(void* arg) {
@@ -14,18 +13,15 @@ void* do_work(void* arg) {
     }
 }
 
-
 int main(int argc, char **argv) {
     int nprocs = atoi(argv[1]);
     pthread_barrier_init(&barrier, NULL, nprocs);
     for (int i=0; i<nprocs; i++) {
         pthread_create(&threads[i], NULL, do_work, i);
     }
-
     for (int i=0; i<nprocs; i++) {
         pthread_join(threads[i], NULL);
     }
-
     pthread_barrier_destroy(&barrier);
 }
 
